7d9ed2b8f4243076a7455cc72e5ef789fa551ee6
Miguel Angel Leyva <migcube@gmail.com>
2021-01-23 10:19:44 -0600

    Se cargo el modelo del  monito partido

--- Projecto_OpenGL/Projecto_OpenGL/brain.cpp
+++ Projecto_OpenGL/Projecto_OpenGL/brain.cpp
@@ -316,19 +316,11 @@ int main()
 	humano = Model();
 	humano.LoadModel("Models/humano.obj");
 
-	/*cabeza = Model();
-	cabeza.LoadModel("Models/cabeza.obj");*/
+	cabeza = Model();
+	cabeza.LoadModel("Models/cabeza.obj");
 
 	onda = Model();
 	onda.LoadModel("Models/onda_sonido.obj");
-
-	//oreja = Model();
-	//oreja.LoadModel("Models/oreja.obj");
-	
-	//cerebro = Model();
-	//cerebro.LoadModel("Models/cerebro.obj");
-
-
 
 
 	/*---------------------------------------LUCES--------------------------------------------*/
@@ -517,9 +509,18 @@ int main()
 		Material_brillante.UseMaterial(uniformSpecularIntensity, uniformShininess);
 		meshList[2]->RenderMesh();
 
-		// Radio y Cabeza
+		// Radio y Humano
 		model = glm::mat4(1.0);
 		model = glm::translate(model, glm::vec3(20.0f, -2.0f, 20.0f));
+		model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		pisoTexture.UseTexture();
+		Material_brillante.UseMaterial(uniformSpecularIntensity, uniformShininess);
+		meshList[2]->RenderMesh();
+
+		//Onda de sonido y cabeza
+		model = glm::mat4(1.0);
+		model = glm::translate(model, glm::vec3(0.0f, -2.0f, 20.0f));
 		model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));
 		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
 		pisoTexture.UseTexture();
@@ -589,13 +590,13 @@ int main()
 
 		/*--------------------------------------- CABEZA --------------------------------------------*/
 
-		//model = glm::mat4(1.0);
-		//model = glm::translate(model, glm::vec3(15.0f, 2.0f, 30.0f));
-		//model = glm::scale(model, glm::vec3(2.0f, 2.0f, 2.0f));
-		//model = glm::rotate(model, 180 * toRadians, glm::vec3(0.0f, 1.0f, 0.0f));
-		//glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
-		//Material_brillante.UseMaterial(uniformSpecularIntensity, uniformShininess);
-		//cabeza.RenderModel();
+		model = glm::mat4(1.0);
+		model = glm::translate(model, glm::vec3(0.0f, 1.0f, 20.0f));
+		model = glm::scale(model, glm::vec3(2.0f, 2.0f, 2.0f));
+		model = glm::rotate(model, 180 * toRadians, glm::vec3(0.0f, 1.0f, 0.0f));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		Material_brillante.UseMaterial(uniformSpecularIntensity, uniformShininess);
+		cabeza.RenderModel();
 
 		/*---------------------------------------Onda --------------------------------------------*/
 		//				movCoche += movOffset*deltaTime;
